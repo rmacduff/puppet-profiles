036ced39bc69b2581505ce49924ccd7a9716b39a
Ross Macduff <ross@vmfarms.com>
2015-08-18 06:11:21 -0400

    specify relahost port and link ca-bundle

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -1,7 +1,14 @@
 class profiles::mail {
   class { 'mandrill': 
-   username    => hiera('mandrill::username'),
-   apikey      => hiera('mandrill::apikey'),
-   mail_domain => hiera('mandrill::mail_domain'),
+   username       => hiera('mandrill::username'),
+   apikey         => hiera('mandrill::apikey'),
+   mail_domain    => hiera('mandrill::mail_domain'),
+   relayhost_port => hiera('mandrill::relayhost_port'),
+  }
+
+  # The mandrill module thinks it's on a debian system
+  file { '/etc/ssl/certs/ca-certificates.crt':
+    ensure => link,
+    target => '/etc/pki/tls/certs/ca-bundle.crt',
   }
 }
