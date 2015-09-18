4ed42eeb185a136e3dcf37162c046c4d703dd970
Ross Macduff <ross.macduff@gmail.com>
2015-09-18 05:31:05 -0400

    move certs into ghost wrapper

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -9,20 +9,6 @@ class profiles::nginx {
     server_tokens    => 'off',
   }
 
-  file { '/etc/pki/tls/certs/_.macduff.ca-bundle.crt':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0444',
-    content => hiera('_.macduff.ca::certificate'),
-  }
-  file { '/etc/pki/tls/private/_.macduff.ca.key':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0444',
-    content => hiera('_.macduff.ca::private_key'),
-  }
   file { '/etc/pki/tls/certs/dhparams.pem':
     ensure => file,
     owner  => root,
@@ -36,5 +22,4 @@ class profiles::nginx {
     proto  => tcp,
     action => accept,
   }
-
 }
