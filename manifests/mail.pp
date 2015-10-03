2e78057fccf565eda6672d2d4078040899788e4f
Ross Macduff <ross.macduff@gmail.com>
2015-10-03 17:35:51 -0400

    now using own postfix module

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -1,14 +1,8 @@
 class profiles::mail {
-  class { 'mandrill': 
-   username       => hiera('mandrill::username'),
-   apikey         => hiera('mandrill::apikey'),
-   mail_domain    => hiera('mandrill::mail_domain'),
-   relayhost_port => hiera('mandrill::relayhost_port'),
-  }
+  include ::postfix_rdm
 
   # The mandrill module thinks it's on a debian system
   file { '/etc/ssl/certs/ca-certificates.crt':
-    ensure => link,
-    target => '/etc/pki/tls/certs/ca-bundle.crt',
+    ensure => absent,
   }
 }
