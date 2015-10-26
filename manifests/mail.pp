2868c8d711df36d52a16c5cbefd8178233e657bc
Ross Macduff <ross.macduff@gmail.com>
2015-10-26 05:30:00 -0400

    symlink has been removed

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -1,8 +1,3 @@
 class profiles::mail {
   include ::postfix_rdm
-
-  # The mandrill module thinks it's on a debian system
-  file { '/etc/ssl/certs/ca-certificates.crt':
-    ensure => absent,
-  }
 }
